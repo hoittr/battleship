408f7fb2d1aee373b878b049a873a6c371465511
Ryan <ryan@comsecpros.com>
2017-08-03 17:43:41 -0700

    Added intro comments to main.c

---
+++ main.c
@@ -0,0 +1,29 @@
+/*
+
+CS-133U-0-33547 - C Programming
+Professor Janik
+
+Group Project
+    Landon Winstead -
+    Ryan Hoitt - ryan.hoitt@pcc.edu
+    Samantha Cleary -
+
+Battleship game for CS133U
+
+https://github.com/hoittr/battleship/
+
+Project: We will be programming a single player “Battleship” game.
+
+In Scope
+- Single Player vs Computer
+- Automatic or Random placement of ships
+- 10x10 Game Board (Graphical Text Representation)
+- Computer AI will be simple RNG + Logic
+- In-game help screen & guidance
+
+Out of Scope
+- Dual Player (Could do this if we have time.)
+- Adaptive AI
+- Save & resume function
+
+*/
