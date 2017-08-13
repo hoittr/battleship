75c5501add045e73a8121fe96869e851cff720b2
SamazonRex <30714693+SamazonRex@users.noreply.github.com>
2017-08-13 15:15:55 -0700

    Added Player Input
    Player input not yet tied to error checking or to entering values into playerboard.

--- main.c
+++ main.c
@@ -32,12 +32,50 @@ Out of Scope
 #include "placement.h"
 #include "drawboard.h"
 #include <time.h>
+#include <ctype.h>
 
 int AIPlacement(int (*boardaddr)[10][10], int ship); // Placement FUNCTION
 
+
 int main() {
+  char playerName [20];
+  char dir;
   time_t t;
+  int row=0, col=0, val=0, ship, i =0;
   srand((unsigned) time(&t)); // Init the RNG
+  
+/* Welcome / Prompt User for Name */
+printf("\t*** BATTLESHIP ***\n");
+printf("Please enter your name: ");
+scanf ("%s", &playerName);
+printf ("Welcome to BATTLESHIP, %s!\n", playerName);
+	
+
+//Player Board Setup
+int *setup; // sets up player board.
+printf("\n#\tClass of ship\tSize\n");
+printf("1\tCarrier \t5\n");
+printf("2\tBattleship \t4\n");
+printf("3\tCruiser \t3\n");
+printf("4\tSubmarine \t3\n");
+printf("5\tDestroyer \t2\n");
+
+for (ship = 1; ship<6;ship++){
+	printf("\n\t ***** FOR SHIP #%i *****\n", ship);
+	printf("Please indicate the row you would like to place the ship (A-J):\n");
+	printf("> ");
+	scanf("%s", &val);
+	row = toupper(val) - 65; //val stores as ASCII. Subtracting 65 = letter decimal value.
+	printf("Please indicate the column you would like to place the ship (1-10):\n");
+	printf("> ");
+	scanf("%i", &col);
+	printf("Which way would you like your ship to point (N,S,E,W)?\n");
+	printf("> ");
+	scanf("%s", &val);
+	char dir = val;
+	printf("You have selected: %i = ship, %i = row, %i= col, %c= direction", ship, row, col, toupper(dir));
+
+}
 
 //=================================================================
 
@@ -68,7 +106,6 @@ int main() {
 
   drawBoard(boardaddr, boardaddr);
 // =================================================================
-
 
 }
 
